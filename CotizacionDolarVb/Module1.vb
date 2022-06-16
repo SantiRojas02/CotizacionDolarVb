e5285b50a7be4926b23b56e6497910e9642e196c
SantiRojas <sr2897040@gmail.com>
2022-06-16 13:58:04 -0300

    CotizacionDolarVb

---
+++ CotizacionDolarVb/Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main(args As String())
+
+        Dim dolar As Decimal
+        Dim pesos As Decimal
+
+        pesos = 200
+
+        Console.Write("Cantidad de dolar a comprar: ")
+        dolar = Console.ReadLine()
+
+
+
+        dolar = dolar * pesos
+
+
+        Console.WriteLine("Valor a pagar: " & dolar)
+
+        Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
